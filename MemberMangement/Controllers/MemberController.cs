0bbb24d57369a18378c47fb61bb37a69d522edf2
Felix Huang Siao-Yu <felix.huang@titansoft.com.sg>
2019-04-20 19:04:58 +0800

    add edit and create api and html

---
+++ MemberMangement/Controllers/MemberController.cs
@@ -0,0 +1,51 @@
+﻿using MemberMangement.Models;
+using MemberMangement.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberMangement.Controllers
+{
+    public class MemberController : Controller
+    {
+        MemberService memberService = new MemberService();
+        // GET: Member
+        public ActionResult Index()
+        {
+            List<Member> MemberList = new List<Member>();
+            MemberList = memberService.GetAllMember();
+            return View(MemberList);
+        }
+
+        public ActionResult DelectMember(int id)
+        {
+            memberService.DelectMember(id);
+            return RedirectToAction("index");
+        }
+
+        public ActionResult AddNewMember()
+        {
+            return View();
+        }
+//        public ActionResult Edit(int id)
+//        {   
+//            Member FindMember = new Member();
+//            FindMember = memberService.FindMember(id);
+//            return View(FindMember);
+//        }
+
+        [HttpPost]
+        public ActionResult AddNewMember(Member Data)
+        {
+            memberService.AddMember(Data);
+            return RedirectToAction("index");
+        }
+        public ActionResult Edit(int Id)
+        {
+            return View(memberService.GetMemberById(Id));
+        }
+
+    }
+}
---
+++ MemberMangement/Service/MemberService.cs
@@ -0,0 +1,50 @@
+﻿using MemberMangement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using Member = MemberMangement.Models.Member;
+
+namespace MemberMangement.Service
+{
+    public class MemberService
+    {
+        MemberManagementEntities db = new MemberManagementEntities();
+
+        public List<Member> GetAllMember()
+        {
+            return db.Member.ToList();
+        }
+
+        public void  DelectMember(int id)
+        {
+            Member DelectmMember = db.Member.SingleOrDefault(x => x.id == id);
+            db.Member.Remove(DelectmMember);
+            db.SaveChanges();
+        }
+
+        public void AddMember(Member AddMember)
+        {
+            db.Member.Add(AddMember);
+            db.SaveChanges();
+        }
+        public Member GetMemberById(int id)
+        {
+           return db.Member.Find(id); ;
+        }
+
+        public void EditMemberData(Member EditMember)
+        {
+            Member oldMember = GetMemberById(EditMember.id);
+            oldMember.Name = EditMember.Name;
+            oldMember.Age = EditMember.Age;
+            oldMember.Address = EditMember.Address;
+            oldMember.Ted = EditMember.Ted;
+            db.SaveChanges();
+        }
+
+        
+    }
+
+}
